b0abbc13e0bb1619059baaa8d14a825fb64f9e7f
JhalakVarshney <86545902+JhalakVarshney@users.noreply.github.com>
2021-08-01 21:00:56 +0530

    First commit

---
+++ klaus.py
@@ -0,0 +1,108 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Klaus Mikaelson, Please tell me how may I help you. ")
+
+def takeCommand():
+    """
+    it takes microphone input from the user and returns string 
+    output.
+    """
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1  
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        #print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls
+    server.login('jhalakvarshney0811@gmail.com', 'your-password')
+    server.sendmail('jhalakvarshney0811@gmail.com', to, content)
+    server.close()
+ 
+
+if __name__ == "__main__":
+    #speak("Jhalak is a good girl")
+    wishMe()
+    while True:
+    #if 1:
+        query = takeCommand().lower()
+   
+   
+    # logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Ma'am, the time is {strTime}")
+
+        elif 'email to jhalak' in query:
+            try:
+                speak("What should I say")
+                content = takeCommand()
+                to = "jhalakvarshney0811@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry love, I am not able to send this email")
+            
+        elif 'quit' in query:
+            speak("Thanks, Have a nice day")
+            exit()
+
+
+            
